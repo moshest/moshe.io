89aba70b587548f5c35867ffddb0f03573d9a5fd
Moshe Simantov <ms@development.co.il>
2018-10-16 12:14:35 +0300

    add Laurent recommendations + marker dots

--- scripts/main.js
+++ scripts/main.js
@@ -2,14 +2,26 @@
 
 (function switchQuotes() {
   var quotes = document.getElementsByClassName('quote');
+  var dots = document.getElementsByClassName('marker-dot');
   var shuffled = [];
-  var currQuote = 0;
+  var currQuote = quotes.length - 1;
+  var nextQuote = 0;
+  var nextTimeout = null;
   var i, j, x;
 
   for (i = 0; i < quotes.length; i += 1) {
     shuffled.push(quotes[i]);
+    (function(i) {
+      dots[i].addEventListener('click', function () {
+        clearTimeout(nextTimeout);
+
+        nextQuote = i;
+        switchQuotes();
+      });
+    })(i);
   }
   quotes[0].classList.remove('active');
+  dots[0].classList.remove('active');
 
   for (i = shuffled.length - 1; i > 0; i -= 1) {
     j = Math.floor(Math.random() * (i + 1));
@@ -21,18 +33,23 @@
 
   function switchQuotes() {
     var oldIndex = currQuote;
-    currQuote += 1;
-    if (currQuote >= shuffled.length) {
-      currQuote = 0;
+    currQuote = nextQuote;
+
+    nextQuote += 1;
+    if (nextQuote >= shuffled.length) {
+      nextQuote = 0;
     }
 
     shuffled[oldIndex].classList.remove('active');
     shuffled[currQuote].classList.add('active');
 
+    dots[oldIndex].classList.remove('active');
+    dots[currQuote].classList.add('active');
+
     var innerText = shuffled[currQuote].innerText;
     var nextTime = innerText ? innerText.length * 50 + 4e3 : 10e3;
 
-    setTimeout(switchQuotes, nextTime);
+    nextTimeout = setTimeout(switchQuotes, nextTime);
   }
 
   switchQuotes();
